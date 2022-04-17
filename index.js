80176cc79f1efd5189d8f1472484cd1c7668974c
Ewerton <tonbortto@gmail.com>
2022-04-17 16:50:57 -0300

    meus commits

--- index.js
+++ index.js
@@ -2,11 +2,49 @@
 //     alert("mensagem apresentada no carregamento do sistema");
 // }
 
-$(() => {
-//     //Codigo executado no carregamento do sistema
-//     alert("mensagem apresentada no carregamento do sistema");
+function populaTabela() {
+    if (localStorage.getItem("produtos")) {
+        let produtos = JSON.parse(localStorage.getItem("produtos"))
 
- $("#btnSalvar").click(() => {
-     console.log("Função click funcionou")
- })
- })
+        $("#tblProdutos tbody").html("")
+        produtos.forEach((produto) => {
+            $("#tblProdutos tbody").append(`<tr>
+                <td>${produto.nome}</td>
+                <td>${produto.quantidade}</td>
+                <td>${produto.valor}</td>
+                <td style="width: 50px"><button type="button" class="btn btn-info">E</button></td>
+                <td style="width: 50px"><button type="button" class="btn btn-danger">D</button></td>
+            </tr>`)
+        })
+    }
+}
+
+$(() => {
+    //     //Codigo executado no carregamento do sistema
+    populaTabela()
+
+    $("#btnSalvar").click(() => {
+        let produto = {}
+        produto.nome = $("#nmProduto").val()
+        produto.quantidade = $("#quantidade").val()
+        produto.valor = $("#vlrProduto").val()
+
+        let listaProdutos = []
+
+    if (localStorage.getItem("produtos")) {
+        listaProdutos = JSON.parse(localStorage.getItem("produtos"))
+    }
+
+    listaProdutos.push(produto)
+
+    localStorage.setItem("produtos", JSON.stringify(listaProdutos))
+
+    alert("Produto salvo com sucesso!")
+
+    $("#nmProduto").val("")
+    $("#quantidade").val("")
+    $("#vlrProduto").val("")
+
+    populaTabela()
+})
+})
